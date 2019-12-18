e31c31e6becb95d3c6f50def6672bd0801bf56cb
Elliot Sasson <esasson@uci.edu>
2019-12-17 19:38:51 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Elliot Sasson on 12/17/19.
+//  Copyright © 2019 elliotsasson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
